7bbefbf6ebfd7b0eaa811fdd3d46a263bc5c458e
MFVen <mikko.fischer@gmail.com>
2013-07-08 22:27:33 +0000

    Changed web.js v2

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-fs.readFileSync('index.html')
+var buf = fs.readFileSync('index.html')
 
 app.get('/', function(request, response) {
   response.send(buf.toString());
@@ -13,3 +13,4 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
